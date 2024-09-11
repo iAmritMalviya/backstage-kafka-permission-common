96432293ff9b605c31c21b70dd52a44cd49bd0b7
Amrit Malviya <90232293+iAmritMalviya@users.noreply.github.com>
2024-09-11 19:31:58 +0530

    added kafka admin

--- src/permissions/kafkaOperations.ts
+++ src/permissions/kafkaOperations.ts
@@ -4,3 +4,24 @@ export const kafkaAdmin = createPermission({
   name: 'kafka.entity.admin',
   attributes: { action: 'create' },
 });
+
+export const kafkaReadPermission = createPermission({
+  name: 'kafka.entity.read',
+  attributes: { action:'read' },
+});
+
+export const kafkaCreatePermission = createPermission({
+  name: 'kafka.entity.create',
+  attributes: { action:'create' },
+});
+
+
+export const kafkaUpdatePermission = createPermission({
+  name: 'kafka.entity.update',
+  attributes: { action:'update' },
+});
+
+export const kafkaDeletePermission = createPermission({
+  name: 'kafka.entity.delete',
+  attributes: { action:'delete' },
+});
